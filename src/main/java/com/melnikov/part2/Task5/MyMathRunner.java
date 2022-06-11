860294eff200ac8955c7644ff9f621e7ccf1fbbf
debugByPrintln <melnikov.n.89@mail.ru>
2022-06-11 22:49:03 +0300

    HotFix.

--- src/main/java/com/melnikov/part2/Task5/MyMathRunner.java
+++ src/main/java/com/melnikov/part2/Task5/MyMathRunner.java
@@ -3,13 +3,21 @@ package com.melnikov.part2.Task5;
 public class MyMathRunner {
     public static void main(String[] args) {
         System.out.println("Counter: " + MyMath.getCounter());
+
         System.out.println(MyMath.add(3.0, 4.2));
+
         System.out.println("Counter: " + MyMath.getCounter());
+
         System.out.println(MyMath.sub(3.0, 1.5));
+
         System.out.println("Counter: " + MyMath.getCounter());
+
         System.out.println(MyMath.mult(3.0, 4.0));
+
         System.out.println("Counter: " + MyMath.getCounter());
+
         System.out.println(MyMath.div(3.0, 1.5));
+
         System.out.println("Counter: " + MyMath.getCounter());
     }
 }
